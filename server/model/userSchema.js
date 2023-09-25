4e31c1af8c0d6c32cc151dcb7a0f57cd4721a675
Pragati Aliani <pragatialiani2003@gmail.com>
2023-09-25 18:22:03 +0530

    token generation

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,37 +1,58 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
-        required: true
+        required: true,
     },
     phone: {
         type: String,
-        required: true
+        required: true,
     },
     work: {
         type: String,
-        required: true
+        required: true,
     },
     password: {
         type: String,
-        required: true
-    }
+        required: true,
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
 });
 
 //* run this "pre" of "save" function. working as a middleware to hash password before saving.
-userSchema.pre('save', async function (next) {
+userSchema.pre("save", async function (next) {
     //* work only if their is password field
-    if (this.isModified('password')) {
+    if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 12);
     }
     next();
 });
+
+//* GENERATING TOKEN
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let new_token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token: new_token });
+        await this.save();
+        return new_token;
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 //* IT IS WORKING PROPERLY WHEN I WRITE "USER" OR "user" OR "users"
 const User = mongoose.model("users", userSchema);
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 require("../db/db");
 const User = require("../model/userSchema");
@@ -9,42 +10,7 @@ router.get("/", (req, res) => {
     res.send(`We are running from router`);
 });
 
-//* This is promises approach
-// router.post("/register", (req, res) => {
-//     const { name, email, phone, work, password } = req.body;
-
-//     if (!name || !email || !phone || !work || !password) {
-//         return res.status(422).json({ message: "Please fill all the fields" });
-//     }
-
-//     User.findOne({ email: email})
-//         .then((userExists) => {
-//             if (userExists) {
-//                 return res.status(422).json({ message: "User already exists" });
-//             }
-//             //* usually you write ({MongoSchemaNameField: inputName, MongoSchemaEmailField: inputEmail}) but we have both the fields same in this case so we are not gonna write like that.
-//             //* if key and value names are same then you can write just once.
-//             const user = new User({ name, email, phone, work, password });
-
-//             user.save()
-//                 .then(() => {
-//                     res.status(201).json({
-//                         message: "User registered successfully",
-//                     });
-//                 })
-//                 .catch((err) =>
-//                     res.status(500).json({
-//                         message: "failed to register",
-//                         error: err.message,
-//                     })
-//                 );
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
-//* This is async-await approach
+//* FOR REGISTERATION OF A NEW USER
 router.post("/register", async (req, res) => {
     const { name, email, phone, work, password } = req.body;
 
@@ -77,6 +43,7 @@ router.post("/register", async (req, res) => {
     }
 });
 
+//* SIGNING IN A USER
 router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -94,20 +61,24 @@ router.post("/login", async (req, res) => {
 
             if (isMatch) {
                 // Passwords match, user is authenticated
+                const token = await userLogin.generateAuthToken();
+
+                res.cookie("jwtoken", token, {
+                    expires: new Date(Date.now() + 2592000000), //30 days
+                    httpOnly: true //because our localhost isn't secured yet
+                });
+
                 return res
                     .status(200)
-                    .json({ message: "Sign in successfully" });
+                    .json({ message: "Sign in successfully", token: token });
             } else {
                 // Passwords do not match
-                return res
-                    .status(401)
-                    .json({ message: "Invalid credentials (password)" });
+                return res.status(401).json({ message: "Invalid credentials" });
             }
         } else {
             // User with the provided email does not exist
-            return res
-                .status(401)
-                .json({ message: "Invalid credentials (email)" });
+            // * WE SHOULDN'T SEND WHICH CREDENTIAL IS WRONG, IT WILL BE EASY FOR HACKER TO HACK
+            return res.status(401).json({ message: "Invalid credentials" });
         }
     } catch (err) {
         console.error("Error during login:", err);
